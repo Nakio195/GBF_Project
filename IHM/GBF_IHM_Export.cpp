11b5a9ceef25338269ba65b1389d3766f1600825
TMz74 <thomarod@gmail.com>
2015-11-25 13:25:22 +0100

    Création de l'IHM pour l'export

---
+++ IHM/GBF_IHM_Export.cpp
@@ -0,0 +1,12 @@
+#include "GBF_IHM_Export.h"
+#include "ui_GBF_IHM_Export.h"
+
+GBF_IHM_Export::GBF_IHM_Export(QWidget *parent) : QWidget(parent), ui(new Ui::GBF_IHM_Export)
+{
+    ui->setupUi(this);
+}
+
+GBF_IHM_Export::~GBF_IHM_Export()
+{
+    delete ui;
+}
